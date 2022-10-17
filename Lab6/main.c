95619435e310488858bf70578092d3ff9922948a
Ian Wilkey <iwilkey@mail.bradley.edu>
2022-10-16 20:09:45 -0500

    Added Lab6

---
+++ Lab6/main.c
@@ -0,0 +1,201 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/// @file ECE-322/Lab6/solution.c
+/// @brief This embedded C program operates to satisfy the requirements of ECE 322 Lab 6.
+/// @authors Ian Wilkey, Andrew Kamp, Rachel Gottschalk
+/// @date 10/16/2022
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// >>> NOTE FOR GRADER <<<
+// This is a complete refactoring of Dr. Mali's LCD library shell, but all functionality is kept.
+// In some cases, it is even more efficient and practical.
+
+#include <avr/io.h>
+#include <util/delay.h>
+#include <stdbool.h>
+
+#define F_CPU 16000000UL
+
+// Connection specific constants...
+#define DATA_BUS PORTC
+#define COMMAND_BUS PORTB
+#define LCD_RS 0 // PORT B0
+#define LCD_RW 1 // PORT B1
+#define LCD_E 2 // PORT B2
+
+// A refactoring of Dr. Mali's enumeration for selecting the nibble data type...
+typedef enum { 
+    LCD_COMMAND_REGISTER = 0, 
+    LCD_DATA_REGISTER = 1
+} lcdRegister_t;
+
+// Prototypes. As you can see, all of Mali's required functionality is present.
+void lcdPutNibble(lcdRegister_t reg, uint8_t data);
+uint8_t lcdGetDataNibble(void);
+void lcdInit(void);
+void lcdPutChar(char chr);
+void lcdPutString(const char * string);
+void lcdPutCommand(uint8_t command);
+void lcdClear(void);
+void lcdHome(void);
+void lcdCursor(bool verdict);
+void lcdSetCursorXY(uint8_t x, uint8_t y);
+void lcdWait(void);
+
+// Send data to the LCD from the microcontroller...
+void lcdPutNibble(lcdRegister_t reg, uint8_t data) {
+    lcdWait();
+    switch(reg) {
+        case LCD_COMMAND_REGISTER:
+            // Send upper nibble of data, pull enable high...
+            COMMAND_BUS |= 1 << LCD_E;
+            DATA_BUS = (data >> 4) & 0x0f;
+            // Pull enable low to get ready for lower nibble...
+            COMMAND_BUS &= ~(1 << LCD_E);
+            // Send lower nubble of data, pull enable high again...
+            COMMAND_BUS |= 1 << LCD_E;
+            DATA_BUS = data & 0x0f;
+            // Reset enable...
+            COMMAND_BUS &= ~(1 << LCD_E);
+            break;
+        case LCD_DATA_REGISTER:
+            // Pull enable high and register select DATA (RS -> 1)...
+            COMMAND_BUS |= (1 << LCD_E | 1 << LCD_RS);
+            // Gather upper nibble...
+            DATA_BUS = (data >> 4) & 0x0f;
+            COMMAND_BUS &= ~(1 << LCD_E);
+            COMMAND_BUS |= (1 << LCD_E);
+            // Gather lower nibble...
+            DATA_BUS = data & 0x0f;
+            // PULL RS to 0! If this is not done, reading data will NOT work correctly.
+            COMMAND_BUS &= ~(1 << LCD_E | 1 << LCD_RS);
+            break;
+        default:;
+    }
+}
+
+// Request and retrieve data from the LCD unit to the microcontroller...
+uint8_t lcdGetDataNibble(void) {
+    // Init a variable to hold read data...
+    uint8_t out = 0x00;
+
+    // Set C to INPUT to read LCD data...
+    DDRC = 0b11110000;
+    // No pull up resistors...
+    PORTC = DDRC;
+
+    // Pull READ/WRITE high (read)...
+    COMMAND_BUS |= 1 << LCD_RW;
+    // Wait tSP1 >= 30ns (1us is OK)...
+    _delay_us(1);   
+
+    // Pull enable high...
+    COMMAND_BUS |= 1 << LCD_E;
+    // Wait two cycles...
+    asm("nop");
+    asm("nop");
+    // Collect upper nibble of data...
+    out = PINC;
+    // Pull enable low to get ready to read lower nibble...
+    COMMAND_BUS &= ~(1 << LCD_E);
+
+    // Shift the current data read to higher nibble portion and clear lower nibble...
+    out <<= 4;
+    out &= 0xf0;
+
+    // Collect lower nibble...
+    COMMAND_BUS |= 1 << LCD_E;
+    asm("nop");
+    asm("nop");
+    // Place the lower nibble in variable without disturbing upper nibble...
+    out |= (PINC & 0x0f);
+    COMMAND_BUS &= ~(1 << LCD_E);
+    // Wait tHD2 >=10ns (1us is OK)...
+    _delay_us(1);
+    // Wait tHD1 >=10ns (1us is OK)...
+    COMMAND_BUS &= ~(1 << LCD_RW);
+    _delay_us(1);
+
+    // Set C back to output...
+    DDRC = 0x0f;
+    PORTC = ~DDRC;
+    PINC = 0x00;
+
+    return out;
+}
+
+// This function waits for the Busy Flag to go low, ergo, it waits the perfect amount of
+// time to do every delicate-timed function of LCD...
+void lcdWait(void) {
+    uint8_t read;
+    while(true) {
+        read = lcdGetDataNibble();
+        // Read D0 (MSB of read dat nibble = BF) to see if it is low (not busy), otherwise
+        // loop till it is!
+        if(!(read & 0x80)) break;
+    }
+    return;
+}
+
+inline void lcdPutChar(char chr) {
+    lcdPutNibble(LCD_DATA_REGISTER, chr);
+}
+
+inline void lcdPutCommand(uint8_t command) {
+    lcdPutNibble(LCD_COMMAND_REGISTER, command);
+}
+
+inline void lcdClear(void) {
+    lcdPutCommand(0x01);
+}
+
+inline void lcdHome(void) {
+    lcdPutCommand(0x80);
+}
+
+void lcdPutString(const char * str) {
+    while(*str != '\0') {
+        lcdPutChar(*str);
+        ++str;
+    }
+}
+
+void lcdCursor(bool verdict) {
+    if(verdict) lcdPutCommand(0b00001111);
+    else lcdPutCommand(0b00001100);
+}
+
+void lcdSetCursorXY(uint8_t x, uint8_t y) {
+    lcdHome();
+    if(y != 1) lcdPutCommand(0xc0);
+    while(x != 1) {
+        lcdPutCommand(0b00010100);
+        x--;
+    }
+}
+
+void lcdInit(void) {
+    // Set up ports...
+    DDRB = 0b00000111;
+    PORTB = ~DDRB;
+    DDRC = 0x0f;
+    PORTC = ~DDRC;
+
+    _delay_ms(20);
+
+    // Set LCD to 4-bit mode...
+    lcdPutCommand(0b00111100);
+    // Enable 5x7 mode for chars...
+    lcdPutCommand(0x28);
+    // Set cursor on and to blink...
+    lcdCursor(true);
+    // Clear display...
+    lcdClear();
+    // Move the cursor to beginning of first line...
+    lcdHome();
+}
+
+int main(void) {
+    lcdInit();
+    while(true);
+    return 0;
+}
